c6e9199d14c0306b3cae47d56a04038eef20e229
littleprincey <orlandow1928@gmail.com>
2020-12-02 21:17:34 +1300

    migrations done, seeds nearly done

--- server/db/migrations/20201202200550_users.js
+++ server/db/migrations/20201202200550_users.js
@@ -1,7 +1,9 @@
+
 exports.up = function (knex) {
   return knex.schema.createTable('users', table => {
     table.increments('id')
     table.string('name')
+    table.string('password')
     table.string('email')
   })
 }
