ee63d5c79c541f507b4e2f702599402aab40a5f2
htense <hortenserothery@gmail.com>
2020-12-03 12:08:27 +1300

    commented out render in index.js

--- client/index.js
+++ client/index.js
@@ -1,23 +1,25 @@
 import React from 'react'
-import { render } from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import { createStore, applyMiddleware, compose } from 'redux'
-import thunk from 'redux-thunk'
 
-import reducers from './reducers'
+import { render } from 'react-dom'
+// import { BrowserRouter as Router } from 'react-router-dom'
+// import { Provider } from 'react-redux'
+// import { createStore, applyMiddleware, compose } from 'redux'
+// import thunk from 'redux-thunk'
+// import reducers from './reducers'
+
+    // <Provider store={store}>
+    //   <Router>
+        <App />
+    //   </Router>
+    // </Provider>,
 import App from './components/App'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)))
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+// const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)))
 
 document.addEventListener('DOMContentLoaded', () => {
   render(
-    <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
-    </Provider>,
+    <App />,
     document.getElementById('app')
   )
 })
