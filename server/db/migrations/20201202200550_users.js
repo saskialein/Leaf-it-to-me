e611074c80086564404fe0a2c01988c01a14eb5d
student <student@pop-os.localdomain>
2020-12-03 14:30:00 +1300

    add some server routes

--- server/db/migrations/20201202200550_users.js
+++ server/db/migrations/20201202200550_users.js
@@ -3,7 +3,7 @@ exports.up = function (knex) {
   return knex.schema.createTable('users', table => {
     table.increments('id')
     table.string('name')
-    table.string('password')
+    table.binary('password')
     table.string('email')
   })
 }
--- server/db/seeds/users.js
+++ server/db/seeds/users.js
@@ -1,11 +1,17 @@
+const { generateHash } = require('authenticare/server')
 
-exports.seed = function(knex) {
+exports.seed = (knex) => {
   return knex('users').del()
-    .then(function () {
-      return knex('users').insert([
-        {
-          id: 1, name: 'Saskia', password: '', email: ''
-        },
-      ]);
-    });
-};
+    .then(() => Promise.all([
+      generateHash('ian'),
+      generateHash('orlando'),
+      generateHash('123')
+    ]))
+    .then(([ianHash, orlandoHash, hortenseHash]) =>
+      knex('users').insert([
+        { id: 1, username: 'Ian', hash: ianHash },
+        { id: 2, username: 'Orlando', hash: orlandoHash },
+        { id: 3, username: 'Hortense', hash: hortenseHash }
+      ])
+      )
+  }
