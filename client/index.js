96d3b9ba39da20ca1684ed46ee68283f92e02dd5
htense <hortenserothery@gmail.com>
2020-12-03 13:44:31 +1300

    updated reducers so local host works

--- client/index.js
+++ client/index.js
@@ -1,25 +1,24 @@
 import React from 'react'
 
 import { render } from 'react-dom'
-// import { BrowserRouter as Router } from 'react-router-dom'
-// import { Provider } from 'react-redux'
-// import { createStore, applyMiddleware, compose } from 'redux'
-// import thunk from 'redux-thunk'
-// import reducers from './reducers'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import reducers from './reducers'
 
-    // <Provider store={store}>
-    //   <Router>
-        <App />
-    //   </Router>
-    // </Provider>,
 import App from './components/App'
 
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-// const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)))
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)))
 
 document.addEventListener('DOMContentLoaded', () => {
   render(
-    <App />,
+    <Provider store={store}>
+       <Router>
+        <App />
+       </Router>
+     </Provider>,
     document.getElementById('app')
   )
 })
--- server/routes/plantRoutes.js
+++ server/routes/plantRoutes.js
@@ -47,5 +47,4 @@ router.post('/', (req, res) => {
     })
 })
 
-
-
+module.exports = router
