6cb5dfb9e991deec7bcf528781a1edce903315ae
student <student@pop-os.localdomain>
2020-12-03 15:15:30 +1300

    latest

--- client/components/Plant.jsx
+++ client/components/Plant.jsx
@@ -10,6 +10,7 @@ const Plant = () => {
     <>
     <h1>Profile</h1>
       <div className='profile-wrapper'>
+
       <div className='each-profile'>
       <div className='profile-picture'>
           <img src={`https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Noto_Emoji_KitKat_263a.svg/200px-Noto_Emoji_KitKat_263a.svg.png`}/>
@@ -20,6 +21,38 @@ const Plant = () => {
         </div>
         </div>
         </div>
+        
+        <div className='each-profile'>
+      <div className='profile-picture'>
+          <img src={`https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Noto_Emoji_KitKat_263a.svg/200px-Noto_Emoji_KitKat_263a.svg.png`}/>
+        <div className='profile-name-plate'>
+          <p className='profile-name'>Plant!</p>
+          <p className='profile-sign'>feeling fabulous</p>
+          <button>button</button>
+        </div>
+        </div>
+        </div>
+        <div className='each-profile'>
+      <div className='profile-picture'>
+          <img src={`https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Noto_Emoji_KitKat_263a.svg/200px-Noto_Emoji_KitKat_263a.svg.png`}/>
+        <div className='profile-name-plate'>
+          <p className='profile-name'>Plant!</p>
+          <p className='profile-sign'>feeling fabulous</p>
+          <button>button</button>
+        </div>
+        </div>
+        </div>
+        <div className='each-profile'>
+      <div className='profile-picture'>
+          <img src={`https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Noto_Emoji_KitKat_263a.svg/200px-Noto_Emoji_KitKat_263a.svg.png`}/>
+        <div className='profile-name-plate'>
+          <p className='profile-name'>Plant!</p>
+          <p className='profile-sign'>feeling fabulous</p>
+          <button>button</button>
+        </div>
+        </div>
+        </div>
+
     </div>
     </>
    
