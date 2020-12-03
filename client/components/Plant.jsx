5e03b4faad7dd4debd054cf13d94702a28d967c4
htense <hortenserothery@gmail.com>
2020-12-03 14:49:30 +1300

    updated component

--- client/components/Plant.jsx
+++ client/components/Plant.jsx
@@ -7,6 +7,26 @@ const Plant = () => {
    
     <h1>Plant</h1>
     <p>Show a small card of the plant</p>
+
+ 
+    <div className='polaroid-wrapper'>
+         <div className='each-polaroid'>
+          <div className= 'polaroid-image-and-title'>
+              <img src='https://www.doc.govt.nz/globalassets/images/places/northland/waipoua-forest/te-matua-ngahere-walk/walkway-1200.jpg' alt="description"/>
+
+              <div className= 'polaroid-title-div'>
+              <span className= 'polaroid-title'>TITLE WITH DROP SHADOW</span>
+              <div><h5>h5 subtitle & border</h5></div>
+              </div>
+          </div>
+          <div className='polaroid-info'>
+              <div className='polaroid-detail'><h4>h4 text</h4></div>
+              <button>Button</button>
+          </div>
+         </div> {/*end of each walk div*/}
+          
+      </div>{/*end of walk wrapper vdiv*/}
+   
     </div>
   )
 }
