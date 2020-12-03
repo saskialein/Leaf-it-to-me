c9400639a916fceb595a729a0b03fdb221265584
Jessica-Veng <35519041+Jessicaveng@users.noreply.github.com>
2020-12-04 12:53:12 +1300

    Managed to move things around and followed the wireframe

--- client/components/IndividualPlant.jsx
+++ client/components/IndividualPlant.jsx
@@ -3,31 +3,33 @@ import React from "react";
 const IndividualPlant = () => {
   return (
     <div className="individ-plant-main">
-      <h2 className="individ-plant-">Common Name</h2>
-      <h3>species_name</h3>
+      <h2 className="commonName">Common Name</h2>
+      <h3 className="spciesName">species_name</h3>
       <p>Show a large card of the plant + the details</p>
 
-      <img
-        src={`https://www.amara.com/static/uploads/images-2/products/huge/156501/big-cactus-cushion-603972.jpg`}
-      />
-
-      <h3>Plant Care Detail</h3>
-      <p>
-        The details for caring for the plant. Lorem ipsum dolor sit amet
-        consectetur, adipisicing elit. Praesentium inventore sunt facere saepe
-        ratione numquam dicta aut qui voluptatem cupiditate adipisci, nesciunt
-        debitis? Quo aspernatur ut aperiam obcaecati odio ea.
-      </p>
-
-      <ul className="">
-        <li>water</li>
-        <li>light</li>
-        <li>temp</li>
-        <li>humidity</li>
-        <li>soil</li>
-        <li>lvl</li>
-        <li>more</li>
-      </ul>
+      <div className="careDetailWrapper">
+        <img
+          src={`https://www.amara.com/static/uploads/images-2/products/huge/156501/big-cactus-cushion-603972.jpg`}
+        />
+        <div className="careDetail">
+          <h3>Plant Care Detail</h3>
+          <p>
+            The details for caring for the plant. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Praesentium inventore sunt facere
+            saepe ratione numquam dicta aut qui voluptatem cupiditate adipisci,
+            nesciunt debitis? Quo aspernatur ut aperiam obcaecati odio ea.
+          </p>
+          <ul className="">
+            <li>water</li>
+            <li>light</li>
+            <li>temp</li>
+            <li>humidity</li>
+            <li>soil</li>
+            <li>lvl</li>
+            <li>more</li>
+          </ul>
+        </div>
+      </div>
 
       <button>Add Plant to profile</button>
     </div>
