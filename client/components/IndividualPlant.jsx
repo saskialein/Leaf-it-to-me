8574a775c331c6c351ede15a7326ad31c66baa6e
student <student@pop-os.localdomain>
2020-12-03 16:20:48 +1300

    add individual plant html

--- client/components/IndividualPlant.jsx
+++ client/components/IndividualPlant.jsx
@@ -5,8 +5,18 @@ const IndividualPlant = () => {
   return (
     <div>
    
-    <h1>IndividualPlant</h1>
+    <h2>IndividualPlant</h2>
     <p>Show a large card of the plant + the details</p>
+    
+
+      <img src="" alt=""/>
+
+      <h3>Plant Care Detail</h3>
+      <p>The details for caring for the plant. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium inventore sunt facere saepe ratione numquam dicta aut qui voluptatem cupiditate adipisci, nesciunt debitis? Quo aspernatur ut aperiam obcaecati odio ea.
+      </p>
+
+      <button>Add Plant to profile</button>
+
     
     </div>
   )
