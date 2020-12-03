d740dd2df07a830c8bd0ab37bb6054f55928a898
Ian Montgomery <ianmontgomery@posteo.net>
2020-12-03 13:46:37 +1300

    add beginning of auth routes

---
+++ server/routes/auth.js
@@ -0,0 +1,22 @@
+
+const { applyAuthRoutes } = require('authenticare/server')
+
+const express = require('express')
+
+const {
+  userExists,
+  getUserByName,
+  createUser
+} = require('../db/users')
+
+const router = express.Router()
+
+// TODO: create POST routes for /auth/signin and /auth/register
+
+applyAuthRoutes(router, {
+  userExists,
+  getUserByName,
+  createUser
+})
+
+module.exports = router
