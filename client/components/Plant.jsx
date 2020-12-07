ca7042b6add7fdc023201624dbbd0bdb2992e5c3
htense <hortenserothery@gmail.com>
2020-12-07 15:14:38 +1300

    hot fix a tag

--- client/components/Plant.jsx
+++ client/components/Plant.jsx
@@ -23,7 +23,6 @@ const Plant = (props) => {
 
         </div>
         <div className='button-plate'>
-          <Link to={`/plants/${plant.common_name}`}><a>Click me</a></Link>
           <a onClick={handleClick}>Add Me</a>
           <Link to={`/plants/${plant.common_name}`}>More info</Link>
         </div>
