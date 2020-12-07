d2212dbca4fd0ba5149e371ef84ec12cc83bc880
emily-berryman <emilyrberryman@gmail.com>
2020-12-07 17:57:16 +1300

    remove working again

--- client/components/UserPlantList.jsx
+++ client/components/UserPlantList.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import Plant from "./Plant";
+import { removePlant } from '../actions/usersPlants'
 
 
 class UserPlantList extends React.Component {
