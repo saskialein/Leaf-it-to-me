0ec8f5ea54f41506701a184591f13ee7f42a7aa0
littleprincey <orlandow1928@gmail.com>
2020-12-07 15:49:30 +1300

    redirect working

--- client/components/IndividualPlant.jsx
+++ client/components/IndividualPlant.jsx
@@ -1,21 +1,29 @@
 import React from "react"
 import { connect } from 'react-redux'
 import { addPlantToReduxandDb } from '../actions/index'
-import { addPlantToUsersPlantsDB } from '../apis/plants'
+// import { addPlantToUsersPlantsDB } from '../apis/plants'
+import { isAuthenticated } from 'authenticare/client'
+
 
 const IndividualPlant = (props) => {
   const name = props.match.params.name
   const plant = props.plants.find((plant) => plant.common_name == name)
 
   const handleClick = () => {
-    console.log('hi i am handleclick')
     const plantObject = {
         plant_id: plant.id,
         name: 'emily'
       }
-    props.dispatch(addPlantToReduxandDb(plantObject))
-    props.history.push('/plants/saved')
-  } 
+      if (isAuthenticated()) {
+        props.dispatch(addPlantToReduxandDb(plantObject))
+        props.history.push("/plants/saved") 
+      } else {
+          props.history.push("/register")
+      }
+    }
+  
+
+  
 
   return (
     // if plant exists render the plant, otherwise render null (see line 45)
