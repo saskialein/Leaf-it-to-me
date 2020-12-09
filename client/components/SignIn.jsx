e368e66347444a251e920cc98d589907dff4a6b1
Saskia Leinberger <saskia.leinberger@gmail.com>
2020-12-09 14:58:38 +1300

    sign up form is okay

--- client/components/SignIn.jsx
+++ client/components/SignIn.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { isAuthenticated, signIn } from "authenticare/client";
-
+import { Link } from 'react-router-dom'
 import { baseApiUrl as baseUrl } from "../config";
 import { checkAuth } from "../actions/auth";
 
@@ -34,33 +34,45 @@ class SignIn extends React.Component {
 
   render() {
     return (
-      <div>
-        <h2>Sign in</h2>
-        <form className="form" onSubmit={this.handleSubmit}>
-          <label className="btn" htmlFor="name">
-            Username:{" "}
+      <div className="form-wrapper">
+        <h2 >Sign in</h2>
+        <hr/>
+        <div className="">
+          <form className="signin-form" onSubmit={this.handleSubmit}>
+            <div className="section">
+          <label htmlFor="name">
+              Username:
           </label>
+            <br/>
           <input
             onChange={this.handleChange}
             name="username"
             value={this.state.username}
-            type="text"
-          ></input>
-          <br />
-          <label className="btn" htmlFor="name">
-            Password:{" "}
+              type="text"
+              placeholder="Enter username here..."
+              ></input>
+            </div>
+            <br />
+            <div className="section">
+          <label htmlFor="name">
+              Password:
           </label>
+            <br/>
           <input
             onChange={this.handleChange}
             name="password"
             value={this.state.password}
             type="password"
-          ></input>
-          <br />
-          <button className="text-btn" type="submit">
+            placeholder="Enter password here..."
+              ></input>
+            </div>
+           
+          <Link className="navy-link" id="submit-button" type="submit">
             Sign in
-          </button>
-        </form>
+          </Link>
+          
+          </form>
+          </div>
       </div>
     );
   }
